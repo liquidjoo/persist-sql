ed0d59d633bef8dbee3c992bda2bffc3240560b9
justin-s <justin.s@kakaocorp.com>
2022-10-18 18:04:17 +0900

    init : 프로젝트 시작

---
+++ src/main/java/camp/nextstep/edu/persistsql/PersistSqlApplication.java
@@ -0,0 +1,13 @@
+package camp.nextstep.edu.persistsql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersistSqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersistSqlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/camp/nextstep/edu/persistsql/PersistSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package camp.nextstep.edu.persistsql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersistSqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
