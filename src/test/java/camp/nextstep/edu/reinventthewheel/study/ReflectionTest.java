659e37700722430ebe2119fa1a4b43c5117a06dd
배재홍 <zhfxmtkachdt@daum.net>
2023-01-18 22:13:18 +0900

    feat : 요구사항 4 - private field에 값 할당 구현

--- src/test/java/camp/nextstep/edu/reinventthewheel/study/ReflectionTest.java
+++ src/test/java/camp/nextstep/edu/reinventthewheel/study/ReflectionTest.java
@@ -1,10 +1,12 @@
 package camp.nextstep.edu.reinventthewheel.study;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.List;
@@ -16,6 +18,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
 
+    private Car car;
+
+    @BeforeEach
+    void setUp() {
+        car = new Car("test", 10000);
+    }
+
     @Test
     @DisplayName("Car 객체 정보 가져오기")
     void showClass() {
@@ -26,8 +35,6 @@ public class ReflectionTest {
     @Test
     @DisplayName("test로 시작하는 함수 실행")
     void testMethodRun() throws Exception {
-
-        Car car = new Car("test", 10000);
         Method[] methods = car.getClass().getDeclaredMethods();
         List<String> filteredMethods = Arrays.stream(methods)
             .filter(method -> method.getName().startsWith("test"))
@@ -40,7 +47,6 @@ public class ReflectionTest {
     @Test
     @DisplayName("@PrintView 애노테이션 메소드 실행")
     public void runPrintView() {
-        Car car = new Car("test", 10000);
         Method[] methods = car.getClass().getDeclaredMethods();
         List<String> filteredMethods = Arrays.stream(methods)
             .filter(method -> method.isAnnotationPresent(PrintView.class))
@@ -48,5 +54,18 @@ public class ReflectionTest {
             .collect(Collectors.toList());
 
         assertThat(filteredMethods).contains("printView");
+    }
+
+    @Test
+    void privateFieldAccess() throws NoSuchFieldException, IllegalAccessException {
+        Field carName = car.getClass().getDeclaredField("name");
+        Field carPrice = car.getClass().getDeclaredField("price");
+        carName.setAccessible(true);
+        carPrice.setAccessible(true);
+        carName.set(car, "hello");
+        carPrice.set(car, 100000);
+
+        assertThat(car.getName()).isEqualTo("hello");
+        assertThat(car.getPrice()).isEqualTo(100000);
     }
 }
