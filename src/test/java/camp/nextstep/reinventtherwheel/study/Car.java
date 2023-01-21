32e93f739c21c1c6c6d8d42fb592536218c65197
GyeongrokKim <kr.kim@mfort.co.kr>
2023-01-21 15:44:26 +0900

    feat: Car 클래스에 테스트 메서드들 구현

--- src/test/java/camp/nextstep/reinventtherwheel/study/Car.java
+++ src/test/java/camp/nextstep/reinventtherwheel/study/Car.java
@@ -2,12 +2,26 @@ package camp.nextstep.reinventtherwheel.study;
 
 public class Car {
     private final String name;
+    private final int price;
 
-    public Car(String name) {
+    public Car(String name, int price) {
         this.name = name;
+        this.price = price;
+    }
+
+    public String testGetName() {
+        return "test : " + name;
+    }
+
+    public String testGetPrice() {
+        return "test : " + price;
     }
 
     public String getName() {
         return name;
+    }
+
+    public int getPrice() {
+        return price;
     }
 }
