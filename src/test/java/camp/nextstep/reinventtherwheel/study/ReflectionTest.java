a099f9c7ec7048047faf63408d7fe948ad89ba7e
GyeongrokKim <kr.kim@mfort.co.kr>
2023-01-21 15:54:37 +0900

    test: test로 시작하는 메소드 실행 테스트 추가

--- src/test/java/camp/nextstep/reinventtherwheel/study/ReflectionTest.java
+++ src/test/java/camp/nextstep/reinventtherwheel/study/ReflectionTest.java
@@ -6,7 +6,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
@@ -24,5 +30,31 @@ public class ReflectionTest {
         for (Field field : carClass.getDeclaredFields()) {
             logger.debug(field.getName());
         }
+    }
+
+    @Test
+    @DisplayName("test로 시작하는 메소드 실행")
+    void testMethodRun() {
+        Class<Car> carClass = Car.class;
+
+        Car car = new Car("제네시스g80", 10_000);
+        List<Method> testMethods = Arrays.stream(carClass.getMethods())
+                .filter(method -> method.getName().startsWith("test"))
+                .collect(Collectors.toList());
+
+        List<String> results = testMethods.stream()
+                .map(it -> {
+                    try {
+                        return (String) it.invoke(car);
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
+                    } catch (InvocationTargetException e) {
+                        e.printStackTrace();
+                    }
+                    return null;
+                })
+                .collect(Collectors.toList());
+
+        assertThat(results).contains("test : 제네시스g80", "test : 10000");
     }
 }
