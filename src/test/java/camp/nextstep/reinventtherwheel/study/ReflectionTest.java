6d5755629f8159f77c36a79e432be8685132a3ff
GyeongrokKim <kr.kim@mfort.co.kr>
2023-01-21 15:59:06 +0900

    test: @PrintView 애노테이션 메소드 실행 테스트 추가

--- src/test/java/camp/nextstep/reinventtherwheel/study/ReflectionTest.java
+++ src/test/java/camp/nextstep/reinventtherwheel/study/ReflectionTest.java
@@ -57,4 +57,23 @@ public class ReflectionTest {
 
         assertThat(results).contains("test : 제네시스g80", "test : 10000");
     }
+
+    @Test
+    @DisplayName("@PrintView 애노테이션 메소드 실행")
+    void testAnnotationMethodRun() {
+        Class<Car> carClass = Car.class;
+        Car car = new Car("제네시스g80", 10_000);
+
+        Arrays.stream(carClass.getMethods())
+                .filter(method -> method.isAnnotationPresent(PrintView.class))
+                .forEach(it -> {
+                    try {
+                        it.invoke(car);
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
+                    } catch (InvocationTargetException e) {
+                        e.printStackTrace();
+                    }
+                });
+    }
 }
